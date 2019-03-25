172be9c78fc6112cb43619d8cc299f9d4d281cca
Michel Dávalos <michel.davalos@academicos.udg.mx>
2019-03-25 14:49:35 -0600

    muestra imagenes y precio

---
+++ src/pages/home/home.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+  casas = [];
+
+  constructor(public navCtrl: NavController, 
+    public http: HttpClient) {
+
+      this.http.get('/v1/klfst?&category=1040&region=16&lang=es&lim=15')
+      .subscribe( data => {
+        
+        this.casas = data['list_ads'];
+        console.log(JSON.stringify(this.casas));
+      }, error => {
+        console.log(JSON.stringify(error));
+      });
+
+  }
+
+}
